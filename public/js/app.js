b14497d0adb723b1b78f59c3466363665ba9f0bf
BrittanyBranson <brittanydibble@Brittanys-MacBook-Pro.local>
2016-04-14 12:16:52 -0400

    modifying different views

--- public/js/app.js
+++ public/js/app.js
@@ -5,19 +5,46 @@
   .module("appOfHonor", [
     "ui.router"
   ])
-  .config(Router);
+  .config(Router)
+  .controller("monthsIndexController", monthsIndexCtrl);
+  .controller("monthsShowController", monthsShowCtrl);
+
   Router.$inject = ["$stateProvider", "$locationProvider", "$urlRouterProvider"];
   function Router($stateProvider, $locationProvider, $urlRouterProvider){
     $locationProvider.html5Mode(true);
     $stateProvider
-    .state("home", {
+    .state("monthsIndex", {
       url: "/",
-      template: "<h2>This is the Home html and it is working</h2>"
+      templateUrl: "/html/months-index.html",
+      controller: "monthsIndexController",
+      controllerAs: "mIndexVM"
     })
-    .state("test",{
-      url: "/test",
-      template: "<h2>This is the Test html and it is working</h2>"
+    .state("monthsShow",{
+      url: "/months/:name",
+      templateUrl: "/html/months-show.html",
+      controller: "monthsShowController",
+      controllerAs: "mShowVM"
     });
     $urlRouterProvider.otherwise("/");
+  }
+
+  function monthsIndexCtrl(){
+    var vm = this;
+    vm.months = [
+      {name: "10-12 Months Out"},
+      {name: "6-9 Months Out"},
+      {name: "3-5 Months Out"},
+      {name: "2 Months Out"},
+      {name: "1 Month Out"},
+      {name: "2 Weeks Out"},
+      {name: "1 Week Out"},
+      {name: "Night Before"},
+      {name: "Day Of"}
+    ];
+  }
+
+  function monthsShowCtrl(){
+    var vm = this;
+    vm.month = "may";
   }
 })();
