4f1b6e886b17109ef8fc55e94d0c0320a63a0bd8
BrittanyBranson <brittanydibble@Brittanys-MacBook-Pro.local>
2016-04-14 11:29:22 -0400

    added state, location and urlroute providers

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var app = express();
 app.use("/", express.static("public"));
 app.use("/", express.static("bower_components"));
 
-app.get("/", function(req, res){
+app.get("/*", function(req, res){
   res.sendFile(__dirname + "/public/index.html");
 });
+
 
 app.listen(3001, function(){
   console.log("Something's working!");
--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,22 @@
 
 (function(){
   angular
-  .module("appOfHonor", []);
-
+  .module("appOfHonor", [
+    "ui.router"
+  ])
+  .config(Router);
+  Router.$inject = ["$stateProvider", "$locationProvider", "$urlRouterProvider"];
+  function Router($stateProvider, $locationProvider, $urlRouterProvider){
+    $locationProvider.html5Mode(true);
+    $stateProvider
+    .state("home", {
+      url: "/",
+      template: "<h2>This is the Home html and it is working</h2>"
+    })
+    .state("test",{
+      url: "/test",
+      template: "<h2>This is the Test html and it is working</h2>"
+    });
+    $urlRouterProvider.otherwise("/");
+  }
 })();
